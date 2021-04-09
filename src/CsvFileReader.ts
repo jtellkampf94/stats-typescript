a9b28ae9cce4a3dfcfeb942a0ba37dada687d12e
Jonathan <jtellkampf@gmail.com>
2021-04-09 16:08:36 +0100

    Refctored csvfilereader class to an abstract class, created match reader class

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,9 @@
-import { MatchResult } from "./MatchResult";
-import { dateStringTodate } from "./utils";
 import fs from "fs";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
   constructor(public filename: string) {}
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -17,18 +14,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringTodate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6]
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
