6ffdb5e39b0c884bf28a0b7474d4d8593aeaca4f
Jonathan <jtellkampf@gmail.com>
2021-04-09 14:59:25 +0100

    Made csvfilereader class parse the matchdata array into its appropriate type

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,7 +1,11 @@
+import { MatchResult } from "./MatchResult";
+import { dateStringTodate } from "./utils";
 import fs from "fs";
 
+type MatchData = [Date, string, string, number, number, MatchResult, string];
+
 export class CsvFileReader {
-  data: string[][] = [];
+  data: MatchData[] = [];
   constructor(public filename: string) {}
 
   read(): void {
@@ -12,6 +16,19 @@ export class CsvFileReader {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      });
+      })
+      .map(
+        (row: string[]): MatchData => {
+          return [
+            dateStringTodate(row[0]),
+            row[1],
+            row[2],
+            parseInt(row[3]),
+            parseInt(row[4]),
+            row[5] as MatchResult,
+            row[6]
+          ];
+        }
+      );
   }
 }
